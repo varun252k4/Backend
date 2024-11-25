1bca050d1602e9e94ec3828d410eb55e136fafd8
varun252k4 <varun.252k4@gmail.com>
2024-11-26 00:27:03 +0530

    CORS added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 import joblib
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 import numpy as np
